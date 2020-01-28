22d364748a9467d69f0abeec74df9007af69b064
humbertopeluso <humbertopeluso@yahoo.com.br>
2020-01-28 16:28:10 -0300

    Command

---
+++ src/com/company/cap7/Comando.java
@@ -0,0 +1,6 @@
+package com.company.cap7;
+
+public interface Comando {
+
+    void executa();
+}
---
+++ src/com/company/cap7/Status.java
@@ -0,0 +1,11 @@
+package com.company.cap7;
+
+public enum Status {
+
+    NOVO,
+    PROCESSANDO,
+    PAGO,
+    ITEM_SEPARADO,
+    ENTREGUE;
+
+}
